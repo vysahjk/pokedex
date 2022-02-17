2d333ddd2078e972c3a8a209019061c327ede171
vysahjk <vysahjk@gmail.com>
2022-02-17 13:58:44 +0100

    add comment in FetchPokemonBy.ts

--- Components/Service/FetchPokemonBy.ts
+++ Components/Service/FetchPokemonBy.ts
@@ -1,5 +1,9 @@
 import axios from "axios";
 
+/**
+ * @param value // value can be "pokemon" or "pokemon-species"
+ * @param namePokemon
+ */
 async function fetchPokemonBy(value: string, namePokemon: string){
     return await axios.get(`/${value}/${namePokemon}`)
 }
